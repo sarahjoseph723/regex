d9254668dc572c5b5c4bdf3bee0294a54a76ed30
Chris Grant <chris@chrisgrant.co>
2015-11-19 11:47:31 -0500

    um

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request, session, redirect
-import urllib2, json, google, bs4, re
+import json, google, re
+from pyquery import PyQuery as pq
 
 app = Flask(__name__)
 
-@app.route("/")
-def go():
-    pass
+@app.route('/', methods=['GET'])
+def main():
+    q = request.args.get('user')
+    if q != '':
+			print 'yo'
+    else: 
+			results = []
+			i = 0
+			print google.search(q, num=10, start=0, stop=10)
+			#for r in google.search(q, num=10, start=0, stop=10):
+			#	d = pq(url=r)
+			#	results[i] = d.html()
+			#	i+=1
+
+			print results
+
+			 
+
+
 
 if __name__=="__main__":
     app.debug = True
